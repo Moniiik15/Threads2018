cbb3c74ac28622ef1eb66c8d9ce382fc415a3c43
Moniiik15 <monikamilenkovic15@gmail.com>
2018-05-20 00:23:09 +0200

    Dodata treca nit "Choir".

--- src/music/Synchronizer.java
+++ src/music/Synchronizer.java
@@ -7,10 +7,12 @@ package music;
 public class Synchronizer {
     
     private boolean firstVoiceFlag;
-
+    private boolean secondVoiceFlag;
+    
     public Synchronizer(boolean firstVoiceFlag) {
         super();
         this.firstVoiceFlag = firstVoiceFlag;
+        this.secondVoiceFlag=!firstVoiceFlag;
     }
     
     public synchronized void singFirstVoice(String lyrics, int delay) {
@@ -26,7 +28,7 @@ public class Synchronizer {
     }
     
     public synchronized void singSecondVoice(String lyrics, int delay) {
-        while (firstVoiceFlag) {
+        while (!secondVoiceFlag) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -37,6 +39,21 @@ public class Synchronizer {
         sing(lyrics, delay);
     }
     
+    public synchronized void singChoir(String lyrics, int delay) {
+    	while(firstVoiceFlag || secondVoiceFlag) {
+    		try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		
+    	}
+    	sing(lyrics, delay);
+    	
+    	
+    }
+    
     private void sing(String lyrics, int delay) {
         System.out.println(lyrics);
         try {
@@ -45,7 +62,17 @@ public class Synchronizer {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        if(firstVoiceFlag) {
         firstVoiceFlag = !firstVoiceFlag;
+        secondVoiceFlag=!secondVoiceFlag;
+        }
+        else if(secondVoiceFlag) {
+        	secondVoiceFlag=!secondVoiceFlag;
+        	
+        }
+        else {
+        	firstVoiceFlag=!firstVoiceFlag;	
+        }
         notifyAll();
     }
 
--- src/test/Test.java
+++ src/test/Test.java
@@ -17,19 +17,21 @@ public class Test {
     
     private Singer pattiSmith;
     private Singer bruceSpringsteen;
+    private Singer choir;
     
     private void initializeSingingInThreads() {
         String lyrics1 = "Because the night";
         String lyrics2 = "Belongs to lovers";
-        
+        String lyrics3= "Because the night belongs to lust";
         boolean stopIt = false;
         Synchronizer synch = new Synchronizer(true);
         
         Performance firstVoicePerformance = new Performance(lyrics1, 1500);
         Performance secondVoicePerformance = new Performance(lyrics2, 1500);
-        
+        Performance choirPerformance = new Performance(lyrics3, 1500);
         pattiSmith = new Singer("Patti Smith", Voice.FIRST, firstVoicePerformance, stopIt, synch);
         bruceSpringsteen = new Singer("Bruce Springsteen", Voice.SECOND, secondVoicePerformance, stopIt, synch);
+        choir = new Singer("Choir", Voice.BACKGROUND, choirPerformance, stopIt, synch);
     }
     
     public void testSingInThreads() {
@@ -38,10 +40,11 @@ public class Test {
         
         pattiSmith.start();
         bruceSpringsteen.start();
-        
+        choir.start();
         IN.nextLine();
         pattiSmith.setStopIt(true);
         bruceSpringsteen.setStopIt(true);
+        choir.setStopIt(true);
         
     }
     
